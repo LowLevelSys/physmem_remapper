fa518d3319792bbd78aa2c628235c96ed3ce256c
Erik Peter Drexler <E.DREXLER@bgfrei.at>
2024-05-21 19:17:44 +0200

    Removed unecessary frame unwinding, which I instead solved through manually unwinding which should be faster

--- physmem_remapper_rewrite/project/interrupts/interrupts.cpp
+++ physmem_remapper_rewrite/project/interrupts/interrupts.cpp
@@ -82,33 +82,6 @@ namespace interrupts {
         IMAGE_DATA_DIRECTORY* exception_directory = 0;
         _IMAGE_RUNTIME_FUNCTION_ENTRY* runtime_functions = 0;
         uint32_t runtime_function_count = 0;
-        uint32_t runtime_function_index = 0;
-   
-        void log_file_exception_directory_info() {
-            if (!dos_header) {
-                dos_header = (IMAGE_DOS_HEADER*)g_driver_base;
-                nt_headers = (IMAGE_NT_HEADERS64*)(g_driver_base + dos_header->e_lfanew);
-
-                exception_directory = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXCEPTION];
-                runtime_functions = (_IMAGE_RUNTIME_FUNCTION_ENTRY*)(g_driver_base + exception_directory->VirtualAddress);
-                runtime_function_count = exception_directory->Size / sizeof(_IMAGE_RUNTIME_FUNCTION_ENTRY);
-            }
-
-            for (uint32_t i = 0; i < runtime_function_count; ++i) {
-                _IMAGE_RUNTIME_FUNCTION_ENTRY* runtime_function = &runtime_functions[i];
-                UNWIND_INFO* unwind_info = (UNWIND_INFO*)(g_driver_base + runtime_function->UnwindData);
-
-                project_log_info("Function %u:", i);
-                project_log_info("  BeginAddress: %llx", runtime_function->BeginAddress);
-                project_log_info("  EndAddress: %llx", runtime_function->EndAddress);
-                project_log_info("  UnwindData: %llx", runtime_function->UnwindData);
-                project_log_info("  CountOfCodes: %u", unwind_info->CountOfCodes);
-
-                for (uint32_t j = 0; j < unwind_info->CountOfCodes; ++j) {
-                    project_log_info("    UnwindCode[%u].CodeOffset: %u", j, unwind_info->UnwindCode[j].CodeOffset);
-                }
-            }
-        }
 
         project_status get_runtime_function(uint64_t rva, _IMAGE_RUNTIME_FUNCTION_ENTRY*& entry, UNWIND_INFO*& unwind_info, SCOPE_TABLE*& scope_table) {
             if (!dos_header) {
@@ -133,131 +106,19 @@ namespace interrupts {
                 unwind_info = (UNWIND_INFO*)(g_driver_base + entry->UnwindData);
                 scope_table = (SCOPE_TABLE*)((uint64_t)(&unwind_info->UnwindCode[(unwind_info->CountOfCodes + 1) & ~1]) + sizeof(uint32_t));
 
+            get_chained_entry:
                 // If the entry is chained, the real data is right after the unwind info
                 if (unwind_info->Flags & UNW_FLAG_CHAININFO) {
                     entry = (_IMAGE_RUNTIME_FUNCTION_ENTRY*)&unwind_info->UnwindCode[(unwind_info->CountOfCodes + 1) & ~1];
                     unwind_info = (UNWIND_INFO*)(g_driver_base + entry->UnwindData);
                     scope_table = (SCOPE_TABLE*)((uint64_t)(&unwind_info->UnwindCode[(unwind_info->CountOfCodes + 1) & ~1]) + sizeof(uint32_t));
+                    goto get_chained_entry;
                 }
-
-                runtime_function_index = i;
 
                 return status_success;
             }
 
             return status_no_runtime_function_found;
-        }
-
-        uint32_t get_unwind_op_slots(UNWIND_CODE code) {
-            static const UCHAR slot_table[]{
-              1, // UWOP_PUSH_NONVOL
-              2, // UWOP_ALLOC_LARGE (or 3, special cased in lookup code)
-              1, // UWOP_ALLOC_SMALL
-              1, // UWOP_SET_FPREG
-              2, // UWOP_SAVE_NONVOL
-              3, // UWOP_SAVE_NONVOL_FAR
-              2, // UWOP_EPILOG
-              3, // UWOP_SPARE_CODE
-              2, // UWOP_SAVE_XMM128
-              3, // UWOP_SAVE_XMM128_FAR
-              1, // UWOP_PUSH_MACHFRAME
-              3  // UWOP_SET_FPREG_LARGE
-            };
-
-            // UWOP_ALLOC_LARGE
-            if (code.UnwindOp == 1 && code.OpInfo != 0)
-                return 3;
-
-            return slot_table[code.UnwindOp];
-        }
-
-        uint64_t& get_unwind_register(trap_frame_ecode_t* unwind_ctx, uint32_t reg) {
-            if (reg < 4) {
-                return (&unwind_ctx->rax)[reg];
-            }
-            else if (reg > 4) {
-                return (&unwind_ctx->rbp)[reg - 5];
-            }
-            else {
-                return unwind_ctx->rsp;
-            }
-        }
-
-        project_status unwind_stack(trap_frame_ecode_t* unwind_ctx, _IMAGE_RUNTIME_FUNCTION_ENTRY* runtime_function_entry, UNWIND_INFO* unwind_info) {
-            if (!unwind_ctx || !runtime_function_entry || !unwind_info)
-                return status_invalid_parameter;
-
-            // Offset from the start of the function
-            uint64_t code_rva = 0;
-            code_rva = unwind_ctx->rip - g_driver_base - runtime_function_entry->BeginAddress;
-
-            uint32_t i = 0;
-
-            // Find the starting opcode
-            while (i < unwind_info->CountOfCodes && // Ammount of unwinding codes
-                unwind_info->UnwindCode[i].CodeOffset < code_rva) { // Check rva so that you don't inadvertently undo pushes that weren't done
-                i += get_unwind_op_slots(unwind_info->UnwindCode[i]);
-            }
-
-            // Unwind all the opcodes
-            while (i < unwind_info->CountOfCodes) {
-                UNWIND_CODE code = unwind_info->UnwindCode[i];
-
-                switch (code.UnwindOp) {
-                case UWOP_PUSH_NONVOL: {
-                    uint64_t& non_vol = get_unwind_register(unwind_ctx, code.OpInfo);
-                    non_vol = *(uint64_t*)unwind_ctx->rsp;
-                    unwind_ctx->rsp += 8;
-                    i += 1;
-                    break;
-                }
-
-                case UWOP_ALLOC_LARGE: {
-                    if (code.OpInfo) {
-                        unwind_ctx->rsp += *(unsigned long*)(&unwind_info->UnwindCode[i + 1]);
-                        i += 3;
-                    }
-                    else {
-                        unwind_ctx->rsp += unwind_info->UnwindCode[i + 1].FrameOffset * 8;
-                        i += 2;
-                    }
-                    break;
-                }
-
-                case UWOP_ALLOC_SMALL: {
-                    unwind_ctx->rsp += (code.OpInfo + 1) * 8;
-                    i += 1;
-                    break;
-                }
-
-                case UWOP_SET_FPREG: {
-                    unwind_ctx->rsp = get_unwind_register(unwind_ctx, unwind_info->FrameRegister) - unwind_info->FrameOffset * 16;
-                    i += 1;
-                    break;
-                }
-
-                case UWOP_SAVE_NONVOL: {
-                    uint64_t& non_vol = get_unwind_register(unwind_ctx, code.OpInfo);
-                    non_vol = *((uint64_t*)unwind_ctx->rsp + *(uint16_t*)&unwind_info->UnwindCode[i + 1]);
-                    i += 2;
-                    break;
-                }
-
-                case UWOP_SAVE_NONVOL_FAR: {
-                    uint64_t& non_vol = get_unwind_register(unwind_ctx, code.OpInfo);
-                    non_vol = *((uint64_t*)unwind_ctx->rsp + *(uint32_t*)&unwind_info->UnwindCode[i + 1]);
-                    i += 3;
-                    break;
-                }
-
-                case UWOP_EPILOG: {
-                    i += 1;
-                    break;
-                }
-                }
-            }
-            
-            return status_success;
         }
 
         // Tries to dispatch an exception that happened in our driver.
@@ -271,31 +132,23 @@ namespace interrupts {
             trap_frame_ecode_t unwind_ctx = *context;
        
             while (true) {
-                _IMAGE_RUNTIME_FUNCTION_ENTRY* runtime_function_entry = 0;
+                _IMAGE_RUNTIME_FUNCTION_ENTRY* entry = 0;
                 UNWIND_INFO* unwind_info = 0;
                 SCOPE_TABLE* scope_table = 0;
                 uint64_t rva = unwind_ctx.rip - g_driver_base;
 
                 // First get the runtime function including info about it, and if it's
                 // a leaf function continue the search and manually unwind
-                status = get_runtime_function(rva, runtime_function_entry, unwind_info, scope_table);
+                status = get_runtime_function(rva, entry, unwind_info, scope_table);
                 if (status != status_success) {
                     unwind_ctx.rip = *(uint64_t*)unwind_ctx.rsp;
                     unwind_ctx.rsp += 8;
                     continue;
                 }
-
-                // The info has to have an exception handler
-                if (!(unwind_info->Flags & UNW_FLAG_EHANDLER)) {
-                    unwind_ctx.rip = *(uint64_t*)unwind_ctx.rsp;
-                    unwind_ctx.rsp += 8;
-                    continue;
-                }
-
-                // Unwind the stack and undo any pushes that might have
-                // ben done by the prologue/the whole function
-                status = unwind_stack(&unwind_ctx, runtime_function_entry, unwind_info);
-                if (status != status_success) {
+                
+                // The info has to have an exception handler or a termination handler
+                if (!(unwind_info->Flags & UNW_FLAG_EHANDLER) &&
+                    !(unwind_info->Flags & UNW_FLAG_UHANDLER)) {
                     unwind_ctx.rip = *(uint64_t*)unwind_ctx.rsp;
                     unwind_ctx.rsp += 8;
                     continue;
@@ -303,8 +156,8 @@ namespace interrupts {
 
                 // Then walk the the scope table and find the right scope record
                 // and continue excution from the info stored in there
-                for (uint32_t entry = 0; entry < scope_table->Count; ++entry) {
-                    SCOPE_RECORD* scope_record = &scope_table->ScopeRecords[entry];
+                for (uint32_t entry_idx = 0; entry_idx < scope_table->Count; ++entry_idx) {
+                    SCOPE_RECORD* scope_record = &scope_table->ScopeRecords[entry_idx];
                     if (rva >= scope_record->BeginAddress && rva < scope_record->EndAddress) {
 
                         // Restore rip
@@ -320,7 +173,7 @@ namespace interrupts {
 
                 // If we reached here this means that we failed to find a scope record
                 // capable of returning execution in our function, so just move on to
-                // the next frame ig
+                // the next frame
                 unwind_ctx.rip = *(uint64_t*)unwind_ctx.rsp;
                 unwind_ctx.rsp += 8;
             }
