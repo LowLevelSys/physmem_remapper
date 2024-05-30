1e338828a5980772b302ab188fe74cf0fd60d6ce
dword64 <74870446+dword64@users.noreply.github.com>
2024-05-30 08:54:11 +0200

    removed nmipoc project

--- nmiPoc/nmi-base/idt/assembly_declarations.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "idt.hpp"
-
-// Assembly declarations
-extern "C" void asm_nmi_handler(void);
-extern "C" uint16_t __read_cs(void);
-
-// Windows APIs
-extern "C" PLIST_ENTRY PsLoadedModuleList;
