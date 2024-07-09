6d95ce6d17df4ee0315e45789a60ba2f1934706a
Joy Boy <nsi.anyplace816@passinbox.com>
2024-07-09 11:19:10 +0900

    Update main.cpp

--- vext/main.cpp
+++ vext/main.cpp
@@ -1,16 +1,20 @@
 #include "driver/driver_um_lib.hpp"
 #include "proc/process.hpp"
 
+const std::string target_proc = "notepad.exe";
+const std::string target_func_string = "MessageBoxA";
+void* target_func = MessageBoxA;
+
 int main(void) {
-	g_proc = process_t::get_inst("notepad.exe");
+	g_proc = process_t::get_inst(target_proc);
 	if (!g_proc) {
 		log("Failed to init process instance");
 		getchar();
 		return -1;
 	}
 
-	if (!g_proc->trigger_cow_in_target(MessageBoxA)) {
-		log("Failed to trigger cow on MessageBoxA");
+	if (!g_proc->trigger_cow_in_target(target_func)) {
+		log("Failed to trigger cow on %s", target_func_string);
 		getchar();
 		return -1;
 	}
@@ -23,9 +27,10 @@ int main(void) {
 	getchar();
 
 	// If you do not revert the trigger it WILL bsod cause of memory management when the process closes
-	g_proc->revert_cow_trigger_in_target(MessageBoxA);
+	g_proc->revert_cow_trigger_in_target(target_func);
 
-	uint64_t mod_base = g_proc->get_module_base("notepad.exe");
+
+	uint64_t mod_base = g_proc->get_module_base(target_proc);
 	if (!mod_base) {
 		log("Failed to get notepad base");
 		getchar();
