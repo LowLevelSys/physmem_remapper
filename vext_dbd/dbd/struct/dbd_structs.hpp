860a80709ca26fe61e63ae3f15ca92a247ba53d9
dword64 <74870446+dword64@users.noreply.github.com>
2024-07-18 15:33:42 +0200

    updated dbd offsets

--- vext_dbd/dbd/struct/dbd_structs.hpp
+++ vext_dbd/dbd/struct/dbd_structs.hpp
@@ -11,9 +11,9 @@ namespace dbd {
 
     namespace offsets {
         // Global
-        constexpr auto OFFSET_GOBJECTS = 0x0F1ABF00;
-        constexpr auto OFFSET_GWORLD = 0x0F34EF50;
-        constexpr auto OFFSET_GNAMES = 0x0F0E9640;
+        constexpr auto OFFSET_GOBJECTS = 0x0F68F940;
+        constexpr auto OFFSET_GWORLD = 0x0F832B70;
+        constexpr auto OFFSET_GNAMES = 0x0F5CD080;
     };
 }
 
